2c3805087d55be6a46c1e22526af278f5fa031dd
Ethan <82918994+fighter-Ethan@users.noreply.github.com>
2021-05-05 18:37:47 -0500

    Changed command prefix, activity status, added setup

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ import json
 import requests
 import random
 
-client = commands.Bot(command_prefix = "/")
+client = commands.Bot(command_prefix = "!")
 member = discord.Member
 client.remove_command("help")
 
 @client.event
 async def on_ready():
   await client.change_presence(activity = discord.Activity
-  (type = discord.ActivityType.streaming , name = "security to servers | /help"))
+  (type = discord.ActivityType.watching , name = "for hackers | !help"))
   print("Bot is up.")
 
 @client.event
 async def on_guild_join(guild):
-   general = find(lambda x: x.name == 'general',  guild.text_channels)
+   general = find(lambda x: x.name == 'general-chat',  guild.text_channels)
    if general and general.permissions_for(guild.me).send_messages:
-     await general.send('Hello {}!'.format(guild.name))
+     await general.send('Hello {}! Do /setup to set up!'.format(guild.name))
 
+@client.command()
+async def setup(ctx):
+  await ctx.send("Put the ID of the channel that you want to be your verify channel!")
+  verify = await client.wait_for("int")
+  verifyy = int(verify)
+  channel = client.get_channel(verifyy)
+  await channel.send(str("React Here!"))
 
 
 @client.command()
@@ -35,3 +42,4 @@ async def invite(ctx):
 keep_alive()
 TOKEN = os.environ.get("DISCORD_BOT_SECRET")
 client.run(TOKEN)
+
